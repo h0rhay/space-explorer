a03c320f2a797923f73dee9e9227352bb222e5c2
George Clark <george.clark@holidayextras.com>
2024-10-22 10:52:20 +0100

    responsive heading

--- app/components/SpaceFontHeading.tsx
+++ app/components/SpaceFontHeading.tsx
@@ -1,7 +1,7 @@
 const SpaceFontHeading = ({ children }: { children: string }) => {
   return (
     <div className="w-full text-center fixed">
-      <h1 className="text-8xl uppercase font-bold tracking-widest font-MachineStd xtext-shadow-space-heading bg-clip-text text-fill-transparent gradient-text">
+      <h1 className="text-[clamp(3rem,6vw,8rem)] uppercase font-bold tracking-narrownpm run decoration-violet-50 font-MachineStd xtext-shadow-space-heading bg-clip-text text-fill-transparent gradient-text">
         {children}
       </h1>
     </div>
